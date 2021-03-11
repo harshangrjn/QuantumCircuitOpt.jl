665b0ce49e86a1134b992a8ae7f4149f07b6ff5e
harshangrjn <harsha.ngrjn@gmail.com>
2021-03-10 23:14:29 -0700

    constraints file

--- src/constraints.jl
+++ src/constraints.jl
@@ -1,8 +1,3 @@
 #--------------------------------------------------#
 # Add all the constraints of the formulation here  #
 #--------------------------------------------------#
-
-function constraint_QCOModel(m::QCOoptimizer)
-    JuMP.@constraint(m.Qmodel, x[1] + x[2] <= 5)
-    return
-end
