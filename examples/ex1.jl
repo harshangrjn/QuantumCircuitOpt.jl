666c6ae051f57f9e1c90f7decad339d6c261f844
harshangrjn <harsha.ngrjn@gmail.com>
2021-03-11 23:48:20 -0700

    update data.jl

--- examples/ex1.jl
+++ examples/ex1.jl
@@ -7,9 +7,16 @@ include("solver.jl")
 
 # All the user 
 params = Dict{String, Any}(
-"n" => 2, # Number of qubits
-"D" => 3, # Minimum depth of the decomposition (>= 2)
-"instance" => "ibm", 
+"n_qubits" => 2, # Number of qubits
+"D" => 5, # Maximum depth of the decomposition (>= 2)
+
+# Note that, for a given input gate, say H (hadamard), user input should include the gates representations on every qubit, such as H1 and H2. 
+# If you prefer to include the kronecker form of gates appearing on adjacent qubits, you can do so by mentioning H⊗H
+"elementary_gates" => ["H1", "H2", "H⊗H", "cnot_12"], 
+"target_gate" => "cnot_21",
+
+# If you prefer to use Universal gates as inputs (like in IBM architecture), provide the discretization angles here
+"U_gate_discretizations" => [], 
 
 "optimizer" => "cplex",
 "presolve" => true,
@@ -17,10 +24,12 @@ params = Dict{String, Any}(
 "lp_relax" => false,
                             
 # Valid inequalities
-"valid_1" => false, #commutative matrices
+"cuts_1" => false, #commutative matrices
 )
 
 qcm_optimizer = get_solver(params)
+data = QuantumCircuitOpt.get_data(params)
+
 # model_qc = build_QCModel(data,params)
 # results = run_QCModel(model_qc, optimizer = cplex)
 
--- src/data.jl
+++ src/data.jl
@@ -1,10 +1,49 @@
-function get_gates_(n_qubits)
+"""
+Given a vector of input with the names of gates (see examples folder), `get_quantum_gates` function 
+returns the corresponding elementary gates in the three-dimensional complex matrix form. 
+""" 
+function get_quantum_gates(params::Dict{String, Any})
+    n_qubits = params["n_qubits"]
+    input = params["elementary_gates"]
+    M = zeros(Complex{Float64}, 2^n_qubits, 2^n_qubits, length(input))
+    for i=1:length(input)
+        M[:,:,i] = get_full_sized_gate(input[i])
+    end
+    T = get_full_sized_gate(params["target_gate"])
 
-   elementary_gates = zeros(n_qubits,n_qubits,3)
+    return M, T
+end
 
+function get_full_sized_gate(input::String)
+    gates = get_elementary_gates()
+    if input == "H1"
+        return kron(gates["hadamard_H"], gates["I_2"])
+    elseif input == "H2"
+        return kron(gates["I_2"], gates["hadamard_H"])
+    elseif input == "cnot_12"
+        return gates["cnot_12"]
+    elseif input == "cnot_21"
+        return gates["cnot_21"]
+    elseif input == "H⊗H"
+        return kron(gates["hadamard_H"], gates["hadamard_H"])
+    # Add here the other gates from get_elementary_gates
+    else
+        Memento.error(_LOGGER, "Specified input elementary gates or the target gate does not exist in the predefined set of gates.")
+    end
+end
 
-#    elementary_gates[:,:,1] = 
-
+function get_data(params::Dict{String, Any})
+    M, T = get_quantum_gates(params)
+    data = Dict{String, Any}("n_qubits" => params["n_qubits"],
+                             "depth" => params["D"],
+                             "M_complex" => M,
+                             "T_complex" => T,
+                             "elementary_gates" => params["elementary_gates"],
+                             "optimizer" => params["optimizer"],
+                             "presolve" => params["presolve"],
+                             "optimizer_log" => params["optimizer_log"],                           
+                             "lp_relax" => params["lp_relax"])
+    return data
 end
 
 function get_elementary_gates()
@@ -68,7 +107,7 @@ function get_elementary_gates()
     return elementary_gates
 end
 
-function get_pauli_rotation_gates(θ)
+function get_pauli_rotation_gates(θ::Float64)
     #input angles in radians
     @assert 0 <= θ <= π
     R_x = Array{Complex{Float64},2}([cos(θ/2) -(sin(θ/2))im; -(sin(θ/2))im cos(θ/2)])
@@ -82,7 +121,7 @@ function get_pauli_rotation_gates(θ)
     return pauli_rotation_gates
 end
 
-function get_universal_gate(θ, ϕ, λ)
+function get_universal_gate(θ::Float64, ϕ::Float64, λ::Float64)
     #input angles in radians
     @assert 0 <= θ <= π
     @assert 0 <= ϕ <= 2*π
