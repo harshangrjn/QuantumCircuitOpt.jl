073a0c718b98294583732a91c4b5cd3519239ac0
harshangrjn <harsha.ngrjn@gmail.com>
2021-11-11 15:29:56 -0700

    [ci skip] mistake fixed in CiSwap

--- examples/3qubit_gates.jl
+++ examples/3qubit_gates.jl
@@ -7,7 +7,7 @@ function decompose_RX_on_q3()
     "num_qubits" => 3, 
     "maximum_depth" => 3,
     "elementary_gates" => ["U3_3", "Identity"], 
-    "target_gate" => QCO.kron_single_qubit_gate(3, QCO.RXGate(π/4), "q3"),
+    "target_gate" => QCOpt.kron_single_qubit_gate(3, QCOpt.RXGate(π/4), "q3"),
     "objective" => "minimize_depth", 
     "decomposition_type" => "exact",
        
@@ -47,7 +47,7 @@ function decompose_toffoli()
     "num_qubits" => 3, 
     "maximum_depth" => 15,
     "elementary_gates" => ["T_1", "T_2", "T_3", "H_3", "CNot_1_2", "CNot_1_3", "CNot_2_3", "Tdagger_1", "Tdagger_2", "Tdagger_3", "Identity"], 
-    "target_gate" => QCO.ToffoliGate(),
+    "target_gate" => QCOpt.ToffoliGate(),
     "objective" => "minimize_depth", 
     "decomposition_type" => "exact",
 
@@ -67,7 +67,7 @@ function decompose_toffoli_using_kronecker()
     "num_qubits" => 3,
     "maximum_depth" => 12,
     "elementary_gates" => ["T_3", "H_3", "CNot_1_2", "CNot_1_3", "CNot_2_3", "Tdagger_3", "I_1xT_2xT_3", "CNot_1_2xH_3", "T_1xTdagger_2xI_3", "Identity"], 
-    "target_gate" => QCO.ToffoliGate(),
+    "target_gate" => QCOpt.ToffoliGate(),
     "objective" => "minimize_depth", 
     "decomposition_type" => "exact",
 
@@ -87,7 +87,7 @@ function decompose_toffoli_with_controlled_gates()
     "maximum_depth" => 5,
     "elementary_gates" => ["CV_1_3", "CV_2_3", "CV_1_2", "CVdagger_1_3", "CVdagger_2_3", "CVdagger_1_2", "CNot_2_1", "CNot_1_2", "Identity"],
     # "elementary_gates" => ["CV_1_3", "CV_2_3", "CVdagger_1_3", "CNot_1_2", "CNot_2_1", "Identity"], 
-    "target_gate" => QCO.ToffoliGate(),
+    "target_gate" => QCOpt.ToffoliGate(),
     "objective" => "minimize_depth",
     "decomposition_type" => "exact",
     )
@@ -102,7 +102,7 @@ function decompose_CNot_1_3()
     "maximum_depth" => 8,
     # "elementary_gates" => ["CNot_1_2", "CNot_2_3", "Identity"], 
     "elementary_gates" => ["H_1", "H_3", "H_2", "CNot_2_1", "CNot_3_2", "Identity"],
-    "target_gate" => QCO.get_full_sized_gate("CNot_1_3", 3),
+    "target_gate" => QCOpt.get_full_sized_gate("CNot_1_3", 3),
     "objective" => "minimize_depth",
     )
 
@@ -115,7 +115,7 @@ function decompose_FredkinGate()
     "maximum_depth" => 7,
     # Reference: https://doi.org/10.1103/PhysRevA.53.2855
     "elementary_gates" => ["CV_1_2", "CV_2_3", "CV_1_3", "CVdagger_1_2", "CVdagger_2_3", "CVdagger_1_3", "CNot_1_2", "CNot_3_2", "CNot_2_3", "CNot_1_3", "Identity"],
-    "target_gate" => QCO.CSwapGate(), #also Fredkin
+    "target_gate" => QCOpt.CSwapGate(), #also Fredkin
     "objective" => "minimize_depth"
     )
 
@@ -126,11 +126,11 @@ function decompose_toffoli_left()
     # Reference: https://arxiv.org/pdf/0803.2316.pdf 
 
     function target_gate()
-        H_3 = QCO.get_full_sized_gate("H_3", 3)
-        Tdagger_3 = QCO.get_full_sized_gate("Tdagger_3", 3)
-        T_3 = QCO.get_full_sized_gate("T_3", 3)
-        cnot_23 = QCO.get_full_sized_gate("CNot_2_3", 3)
-        CNot_1_3 = QCO.get_full_sized_gate("CNot_1_3", 3)
+        H_3 = QCOpt.get_full_sized_gate("H_3", 3)
+        Tdagger_3 = QCOpt.get_full_sized_gate("Tdagger_3", 3)
+        T_3 = QCOpt.get_full_sized_gate("T_3", 3)
+        cnot_23 = QCOpt.get_full_sized_gate("CNot_2_3", 3)
+        CNot_1_3 = QCOpt.get_full_sized_gate("CNot_1_3", 3)
 
         return H_3 * cnot_23 * Tdagger_3 * CNot_1_3 * T_3 * cnot_23 * Tdagger_3
     end
@@ -151,13 +151,13 @@ function decompose_toffoli_right()
     # Reference: https://arxiv.org/pdf/0803.2316.pdf 
     
     function target_gate()
-        H_3 = QCO.get_full_sized_gate("H_3", 3)
-        Tdagger_2 = QCO.get_full_sized_gate("Tdagger_2", 3)
-        T_1 = QCO.get_full_sized_gate("T_1", 3)
-        T_2 = QCO.get_full_sized_gate("T_2", 3)
-        T_3 = QCO.get_full_sized_gate("T_3", 3)
-        CNot_1_2 = QCO.get_full_sized_gate("CNot_1_2", 3)
-        CNot_1_3 = QCO.get_full_sized_gate("CNot_1_3", 3)
+        H_3 = QCOpt.get_full_sized_gate("H_3", 3)
+        Tdagger_2 = QCOpt.get_full_sized_gate("Tdagger_2", 3)
+        T_1 = QCOpt.get_full_sized_gate("T_1", 3)
+        T_2 = QCOpt.get_full_sized_gate("T_2", 3)
+        T_3 = QCOpt.get_full_sized_gate("T_3", 3)
+        CNot_1_2 = QCOpt.get_full_sized_gate("CNot_1_2", 3)
+        CNot_1_3 = QCOpt.get_full_sized_gate("CNot_1_3", 3)
 
         return CNot_1_3 * T_2 * T_3 * CNot_1_2 * H_3 * T_1 * Tdagger_2 * CNot_1_2
     end
@@ -177,12 +177,12 @@ function decompose_miller()
     # Reference: https://doi.org/10.1109/TCAD.2005.858352
 
     function target_gate()
-        CV_1_3 = QCO.get_full_sized_gate("CV_1_3", 3)
-        CV_2_3 = QCO.get_full_sized_gate("CV_2_3", 3)
-        CVdagger_2_3 = QCO.get_full_sized_gate("CVdagger_2_3", 3)
-        CNot_1_2 = QCO.get_full_sized_gate("CNot_1_2", 3)
-        CNot_3_1 = QCO.get_full_sized_gate("CNot_3_1", 3)
-        CNot_3_2 = QCO.get_full_sized_gate("CNot_3_2", 3)
+        CV_1_3 = QCOpt.get_full_sized_gate("CV_1_3", 3)
+        CV_2_3 = QCOpt.get_full_sized_gate("CV_2_3", 3)
+        CVdagger_2_3 = QCOpt.get_full_sized_gate("CVdagger_2_3", 3)
+        CNot_1_2 = QCOpt.get_full_sized_gate("CNot_1_2", 3)
+        CNot_3_1 = QCOpt.get_full_sized_gate("CNot_3_1", 3)
+        CNot_3_2 = QCOpt.get_full_sized_gate("CNot_3_2", 3)
 
         return CNot_3_1 * CNot_3_2 * CV_2_3 * CNot_1_2 * CVdagger_2_3 * CV_1_3 * CNot_3_1 * CNot_1_2
     end
@@ -206,7 +206,7 @@ function decompose_relative_toffoli()
         "num_qubits" => 3,
         "maximum_depth" => 9,
         "elementary_gates" => ["H_3", "T_3", "Tdagger_3", "CNot_1_2", "CNot_2_3", "CNot_1_3", "Identity"],
-        "target_gate" => QCO.RCCXGate(),
+        "target_gate" => QCOpt.RCCXGate(),
         "objective" => "minimize_depth",
 
         "set_cnot_upper_bound" => 3
@@ -222,7 +222,7 @@ function decompose_margolus()
         "num_qubits" => 3,
         "maximum_depth" => 7,
         "elementary_gates" => ["RY_3", "CNot_1_2", "CNot_2_3", "CNot_1_3", "Identity"],
-        "target_gate" => QCO.MargolusGate(),
+        "target_gate" => QCOpt.MargolusGate(),
         "objective" => "minimize_depth",
 
         "RY_discretization" => -π:π/4:π,
@@ -239,9 +239,9 @@ function decompose_CiSwap()
 
         "num_qubits" => 3,
         "maximum_depth" => 12,
-        # "elementary_gates" => ["H_3", "T_3", "Tdagger_3", "CNot_3_2", "CNot_2_3", "CNot_1_3", "Identity"],
-        "elementary_gates" => ["H_3", "S_3", "Sdagger_3", "CNot_3_2", "CNot_2_3", "CNot_1_3", "Identity"],
-        "target_gate" => QCO.CiSwapGate(),
+        "elementary_gates" => ["H_3", "T_3", "Tdagger_3", "CNot_3_2", "CNot_2_3", "CNot_1_3", "Identity"],
+        # "elementary_gates" => ["S_2", "Sdagger_2", "S_3", "Sdagger_3", "CNot_3_2", "CNot_2_3", "CNot_1_3", "Identity"],
+        "target_gate" => QCOpt.CiSwapGate(),
         "objective" => "minimize_depth",
         )
 
