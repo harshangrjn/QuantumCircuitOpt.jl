688fdea1673997c0fb10ac1087eb50f2c841e15f
szabino-mrszabo <zsoltszabo.szabo@gmail.com>
2022-09-23 17:14:50 -0600

    adding 7q cat state verification code

--- examples/5qubit_gates.jl
+++ examples/5qubit_gates.jl
@@ -17,3 +17,33 @@ function decompose_RX_on_5qubits()
     "RX_discretization" => [π/2],
     )
 end
+
+function verification_of_cat_state()
+
+    println(">>>>> verification_of_cat_state <<<<<")
+ 
+    num_qubits = 7
+
+    function target_gate()
+
+        CNot_4_5 = QCOpt.get_full_sized_gate("CNot_4_5", num_qubits);
+        CNot_3_5 = QCOpt.get_full_sized_gate("CNot_3_5", num_qubits);
+        CNot_3_6 = QCOpt.get_full_sized_gate("CNot_3_6", num_qubits);
+        CNot_2_6 = QCOpt.get_full_sized_gate("CNot_2_6", num_qubits);
+        CNot_2_7 = QCOpt.get_full_sized_gate("CNot_2_7", num_qubits);
+        CNot_1_7 = QCOpt.get_full_sized_gate("CNot_1_7", num_qubits);
+        
+
+        return CNot_4_5 * CNot_3_5 * CNot_3_6 * CNot_2_6 * CNot_2_7 * CNot_1_7
+    end
+    return Dict{String, Any}(
+    
+    "num_qubits" => 7, 
+    "maximum_depth" => 6,
+    "elementary_gates" => ["CNot_1_5", "CNot_2_5", "CNot_2_6", "CNot_3_6", "CNot_3_7", "CNot_4_7", "CNot_5_6", "CNot_5_7", "CNot_6_7" ], 
+    "target_gate" => target_gate(),
+    "objective" => "minimize_depth", 
+    "decomposition_type" => "exact_optimal",
+
+    )
+end
