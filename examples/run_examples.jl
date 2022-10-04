bdd257557b0ab359a32548d7b7d36b28fd77f208
szabino-mrszabo <zsoltszabo.szabo@gmail.com>
2022-10-04 10:34:59 -0600

    getting harsha changes

--- examples/run_examples.jl
+++ examples/run_examples.jl
@@ -10,30 +10,7 @@ include("4qubit_gates.jl")
 include("5qubit_gates.jl")
 include("decompose_all_gates.jl")
 
-<<<<<<< HEAD
-decompose_gates = ["decompose_hadamard",
-                   "decompose_controlled_Z",
-                   "decompose_controlled_V",
-                   "decompose_controlled_H",
-                   "decompose_controlled_H_with_R",
-                   "decompose_magic", 
-                   "decompose_magic_using_SHCnot",
-                   "decompose_S", 
-                   "decompose_revcnot", 
-                   "decompose_revcnot_with_U", 
-                   "decompose_swap",
-                   "decompose_W",
-                   "decompose_W_using_HCnot",
-                   "decompose_GroverDiffusion_using_Clifford",
-                   "decompose_GroverDiffusion_using_U3",
-                   "decompose_iSwap",
-                   "decompose_qft2_using_HT",
-                   "decompose_RX_on_q3"]
-
-decompose_gates = ["decompose_controlled_V"]
-=======
 # decompose_gates = ["iSwap"]
->>>>>>> 381b589ae3f07d25e43e78a63dfa40d287114925
 
 #----------------------------------------------#
 #      Quantum Circuit Optimization model      #
